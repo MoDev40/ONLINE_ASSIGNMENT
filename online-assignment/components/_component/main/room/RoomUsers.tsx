77a3bf6db34feffe518f73d8d7179b10483fd7d0
M40 <modev.404@gmail.com>
2024-06-28 06:32:29 +0300

    ✨ Add RoomUsers Card to RoomPageCard Component

--- online-assignment/components/_component/main/room/RoomUsers.tsx
+++ online-assignment/components/_component/main/room/RoomUsers.tsx
@@ -1,16 +1,32 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { useGetRoomUsersQuery } from "@/lib/features/roomSlice";
 import Loading from "../../Loading";
+import AvatarCard from "../../AvatarCard";
+import { cn } from "@/lib/utils";
 
 type RoomUsersProps = {
     room_id:string;
 }
 const RoomUsers = ({ room_id }:RoomUsersProps) => {
-    const { data:roomUsers, isFetching , isLoading } = useGetRoomUsersQuery(room_id)
+    const { data:roomWithUsers, isFetching , isLoading } = useGetRoomUsersQuery(room_id)
     if(isLoading || isFetching) return <div className="container mx-auto"><Loading/></div>
   return (
-    <div>
-    RoomUsers
-    </div>
+    roomWithUsers&&
+    roomWithUsers.map((room) => (
+      <Card key={room.id} className={
+        cn(
+          "flex flex-row justify-between shadow-sm h-auto items-center gap-2",
+        )
+      }>
+        <CardHeader>
+          <CardTitle>{room.user.name}</CardTitle>
+          <CardDescription>{room.user.idCard}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <AvatarCard fallback={room.user.name?.charAt(0) as string}/>
+        </CardContent>
+      </Card>
+    ))
   )
 }
 
