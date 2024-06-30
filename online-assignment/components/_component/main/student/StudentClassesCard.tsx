c7f267a4574a60cdbb050a698d6882e7ca36712f
M40 <modev.404@gmail.com>
2024-06-30 11:27:46 +0300

    ✨ Update student class card with joinedAt date and classroom assignments
    🚧 Add StudentClassSetting component for student class settings
    
    ✨ Create LeaveRoomForm component for leaving a room

--- online-assignment/components/_component/main/student/StudentClassesCard.tsx
+++ online-assignment/components/_component/main/student/StudentClassesCard.tsx
@@ -10,14 +10,19 @@ const StudentClassesCard = ({ userId }:{ userId:string }) => {
     const { data:studentClasses,isFetching } = useGetStudentRoomsQuery(user?.id as string)
 
     if(isLoading || isFetching) return <div className="container mx-auto"><Loading/></div>
-
+    
+    if(user?.role === "teacher") {
+        window.location.pathname = "/"
+        return null
+    }
+    
     return(
         <div className="flex flex-col container mx-auto">
             <JoinRoomForm student_id={user?.id as string}/>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 container mx-auto xl:grid-cols-4 mt-6 lg:grid-cols-3">
                 {   studentClasses&&
                     studentClasses.map((room)=>(
-                        <StudentClassCard key={room.id} room={room.classroom} />
+                        <StudentClassCard key={room.id} room={room} />
                     ))
                 }
             </div>
