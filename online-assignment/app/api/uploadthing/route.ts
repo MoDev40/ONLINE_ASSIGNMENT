a7225796f1c4d875989c8d7e795397540ec28ac0
M40 <modev.404@gmail.com>
2024-07-22 11:43:21 +0300

    ✨ Add new DELETE route for deleting files and submissions in API.

--- online-assignment/app/api/uploadthing/route.ts
+++ online-assignment/app/api/uploadthing/route.ts
@@ -1,6 +1,8 @@
 import { createRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
+import { NextRequest, NextResponse } from "next/server";
+import { UTApi } from "uploadthing/server";
  
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
@@ -9,3 +11,16 @@ export const { GET, POST } = createRouteHandler({
   // Apply an (optional) custom config:
   // config: { ... },
 });
+
+export async function DELETE(req:NextRequest){
+  try {
+
+    const utApi = new UTApi()
+    const { fileKeys } : { fileKeys : string | string[] } = await req.json()
+    await utApi.deleteFiles(fileKeys)
+    return NextResponse.json("ok",{status:200})
+
+  } catch (error) {
+    return NextResponse.json({message:"unexpected error"},{status:500})
+  }
+}
