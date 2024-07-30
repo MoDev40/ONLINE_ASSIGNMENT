fa4127a648d215fba831e2c2d290162c1b0aad0b
modev <modev.404@gmail.com>
2024-07-30 11:02:54 +0300

    🔧 Fix join room form in StudentClassesCard and handle null values properly

--- online-assignment/components/_component/main/student/StudentClassesCard.tsx
+++ online-assignment/components/_component/main/student/StudentClassesCard.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import Loading from '../../Loading'
 import JoinRoomForm from '../class/JoinRoomForm'
 import StudentClassCard from './StudentClassCard'
+import toast from 'react-hot-toast'
 
 const StudentClassesCard = ({ userId }:{ userId:string }) => {
     const scrolled = useScroll(50);
@@ -17,7 +18,10 @@ const StudentClassesCard = ({ userId }:{ userId:string }) => {
     if(user?.role !== "student") {
         window.location.pathname = "/"
         return null
-    }else if(user?.idCard === "" || user.name === "" || user.className === "") {
+    }
+
+    if(user?.idCard === null || user?.name === null || user?.className === null) {
+        alert("Please complete your profile");
         window.location.pathname = "/profile"
         return null
     }
@@ -35,7 +39,7 @@ const StudentClassesCard = ({ userId }:{ userId:string }) => {
             <Link href='/'>
               <h1 className="relative flex flex-row items-baseline text-2xl font-bold"><span className="sr-only">EDP</span><span className="tracking-tight hover:cursor-pointer">ED<span className="text-primary">P</span></span><sup className="absolute left-[calc(100%+.1rem)] top-0 text-xs font-bold text-black">[BETA]</sup></h1>
             </Link>
-            <JoinRoomForm student_id={user?.id} />
+            <JoinRoomForm student_id={user?.id as string} />
           </section>
         </div>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 container mx-auto xl:grid-cols-4 mt-6 lg:grid-cols-3">
